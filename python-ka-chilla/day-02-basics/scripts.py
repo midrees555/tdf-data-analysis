bdcc9595e49c20fccd34a12d463e80f93e11a092
MUHAMMAD IDREES <midrees4040@gmail.com>
2025-02-19 00:49:31 +0500

    python basics were discussed in script file inside 'day-02-basics'

---
+++ python-ka-chilla/day-02-basics/scripts.py
@@ -0,0 +1,16 @@
+print("--------------------")
+print("Hello World\n")
+
+# Python comments
+# print("Hello Me")
+print("Hello Me")  # A comment is start from # in the start of line.
+# print("Hello Me") # Now this is a comment
+
+# Python variables
+# In other (than python) programming languages, in most cases variables are declare and initilizes when we specify a datatype but in python we don't need to specify a datatype i.e.
+my_name = "MUHAMMAD IDREES"
+my_age = 23
+area_of_a_circle = 10.43
+today_is_rainy = True
+
+# so the above lines are specifying the datatype by itself
