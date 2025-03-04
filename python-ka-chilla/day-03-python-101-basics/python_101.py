57cc9bdd5787a4d1f71240cb491374f06c33015a
MUHAMMAD IDREES <midrees4040@gmail.com>
2025-03-04 23:29:05 +0500

    some files were changed

---
+++ python-ka-chilla/day-03-python-101-basics/python_101.py
@@ -0,0 +1,7 @@
+user_input = input("\nEnter a choice b/w 0 and 1\n")
+
+while(1):
+    if user_input == 1:
+        break
+    print("Hello Musadiq")
+    user_input = input("Enter 1 to exit\n Enter 0 to start again...")
