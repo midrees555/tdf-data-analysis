9e2691260ccaed7100a56d1e9e5b7ccf1af20fd4
MUHAMMAD IDREES <115445554+midrees555@users.noreply.github.com>
2025-04-16 14:51:00 +0000

    Definition of a string

--- python-ka-chilla/day-03/03_strings.py
+++ python-ka-chilla/day-03/03_strings.py
@@ -1,6 +1,30 @@
 print("--------------------------------|")
-print("--------------------------------|\nHello String")
+print("--------------------------------|\nHello String Chapter....")
+print("--------------------------------|")
+
+# What is string.......?
+# Strings are written in single 'string', double "string" or tripple """string""" quotation mark. It (string - collection of words) are printed as it is (Like Someone has said in English language) کسی کی بات کو نقل کرنا
 
 
+print("Hellow World (Error)")       # This is an error in string, and this is cannot be detect and fix by python interpreter by itself but we have to remove manually.... like the below:
+print("Hello World (Correct)")
+
+print('We are learning python with Dr Aammar on Codanics')  # Single Quotation mark....
+
+# Why multi representation used in string?
+print("Hello Learner.......! This is 'MUHAMMAD IDREES'")
+print('''
+--------------------------------
+This is multiline string
+Hey! This is TDF - TechDevFamily Leader!
+I hope this message find you well.
+Take Care
+''')
+
+# As a Professional use We have to use double quotation in most cases
+# `` called bactics and use to show code separately from other text in markdown language. And we will see this in upcoming chapters
+# 
+# ------------------------------------------!
+#    
 
 print("--------------------------------|")
