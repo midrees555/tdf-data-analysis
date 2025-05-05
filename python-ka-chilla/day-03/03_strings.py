c5f87ae693c9aca46e817e1d63828342b88a632f
MUHAMMAD IDREES <115445554+midrees555@users.noreply.github.com>
2025-05-05 17:38:21 +0000

    Assignment was done in file 'string.py'

--- python-ka-chilla/day-03/03_strings.py
+++ python-ka-chilla/day-03/03_strings.py
@@ -38,12 +38,12 @@ And This is a Tripple Double
 
 # Numbering in string
 print(2 + 3)
-print("2 + 3 + 5 - 4")
+print("2+3+5-4")
 
 # What is Type Casting in programming
 # Converting one data type into another, is called type casting
 # Example:
-stringNum = "2 + 3 + 5 + 7 + 9"
+stringNum = "29"
 print(type(stringNum))      # This will probably print <class 'str'> means, variable belongs to 'string class'
 print("----------------------")
 
@@ -58,8 +58,31 @@ except TypeError as e:
 
 # Solution:
 stringNum = int(stringNum)
-print(myAge + stringNum)            # the method 'int()' will convert 'string' into 'int' type
+try:
+    answer = myAge + stringNum            # the method 'int()' will convert 'string' into 'int' type
+    print(f"The Problem is resolved and the answer is\n{answer}")
+
+except TypeError as e:
+    print(f"Something went wrong...\nError Message: {e}")
+
+
 print("----------------------")
+# -------------------------------------------------------------------------------
+
+# Another Type Casting Example:-    from 'int' to 'String'
+myIntVar = 100
+# The below statement  will print <class 'int'>, which means it's belongs to int data type
+print(f"Type of myIntVar = {type(myIntVar)}")
+
+# Conversion 'int' to 'String':-
+myStringVar = str(myIntVar)
+
+# Result Printing...
+print(f"Type of New variable 'myStringVar' = {type(myStringVar)}")
+# Now this will print <class 'str'> which means string data type
+
+# -------------------------------------------------------------------------------
+
 
 
 
